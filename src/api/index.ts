f28361272d5de31d28e1edc182e8d058fba53571
Kian Janloo <ddrdslayer@gmail.com>
2025-06-26 13:15:21 +0330

    Structure Vercel

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import logger from "./utils/logs/logger";
-import errorMiddleware from "./middlewares/error/errorMiddleware";
-import { authController, cartControllers, categoriesController, commentsControllers, dashboardController, favoritesControllers, ordersControllers, paymentsController, productsController, rolesController, scoresControllers, usersController } from "./controllers";
+import logger from "../utils/logs/logger";
+import errorMiddleware from "../middlewares/error/errorMiddleware";
+import { authController, cartControllers, categoriesController, commentsControllers, dashboardController, favoritesControllers, ordersControllers, paymentsController, productsController, rolesController, scoresControllers, usersController } from "../controllers";
 
 const app = express();
 app.use(cors());
@@ -31,3 +31,5 @@ mongoose.connect("mongodb+srv://admin:1388ki8831@express.nlqmdqy.mongodb.net/").
         logger.info("Serve is on")
     });
 });
+
+export default app;
